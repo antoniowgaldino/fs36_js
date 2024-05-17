5039940a6737abe2456e131968e41856132cb0fc
antoniowgaldino <alberto.ce@gmail.com>
2024-05-16 21:53:44 -0300

    Layout de calculadora

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// criando uma variável e atribuindo um valor
+let nome = 'João';
+
+/*
+Declaração de variáveis
+1- não pode ser palavra reservada
+2- não pode começar com número
+3- não pode conter espaços
+4- não pode ter caracteres especiais
+5- não pode iniciar com operadores aritméticos: +, -, /, *
+6- não pode iniciar com operadores de comparação: >, <, =
+*/
