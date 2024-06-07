18cb1f7c9a7c46a63038bf43e58286ccff457fa4
antoniowgaldino <alberto.ce@gmail.com>
2024-06-06 21:50:27 -0300

    enxugando código

--- js/script.js
+++ js/script.js
@@ -1,52 +1,33 @@
-/*
-Declaração de variáveis
-1- não pode ser palavra reservada
-2- não pode começar com número
-3- não pode conter espaços
-4- não pode ter caracteres especiais
-5- não pode iniciar com operadores aritméticos: +, -, /, *
-6- não pode iniciar com operadores de comparação: >, <, =
-*/
+const
+    num1 = document.getElementById('n1');
+    num2 = document.getElementById('n2');
+    conteudoDiv = document.querySelector('#resultado');
 
 function soma() {
-    let //declarando variaveis e convertendo num, de string para number
-        num1 = parseFloat(document.getElementById('n1').value),
-        num2 = parseFloat(document.getElementById('n2').value),
-        result = num1 + num2; //calculando
-
-    // imprmir result no HTML e trocando "." pela ","
-    document.getElementById('resultado').innerHTML = result.toString().replace(".", ",");
+    let result = parseFloat(num1.value) + parseFloat(num2.value);
+    exibirResultado(result);
 }
 
 function subtrai() {
-    let
-        num1 = parseFloat(document.getElementById('n1').value),
-        num2 = parseFloat(document.getElementById('n2').value),
-        result = num1 - num2;
-
-    document.getElementById('resultado').innerHTML = result.toString().replace(".", ",");
+    let result = parseFloat(num1.value) - parseFloat(num2.value);
+    exibirResultado(result);
 }
 
 function multiplica() {
-    let
-        num1 = parseFloat(document.getElementById('n1').value),
-        num2 = parseFloat(document.getElementById('n2').value),
-        result = num1 * num2;
-
-    document.getElementById('resultado').innerHTML = result.toString().replace(".", ",");
+    let result = parseFloat(num1.value) * parseFloat(num2.value);
+    exibirResultado(result);
 }
 
 function divide() {
-    let
-        num1 = parseFloat(document.getElementById('n1').value),
-        num2 = parseFloat(document.getElementById('n2').value),
-        result = num1 / num2;
+    let result = parseFloat(num1.value) / parseFloat(num2.value);
+    exibirResultado(result);
+}
 
-    document.getElementById('resultado').innerHTML = result.toString().replace(".", ",");
+function exibirResultado(result) {
+    conteudoDiv.innerHTML = result.toString().replace(".", ",");
 }
 
 function limpar() {
-    document.getElementById('n1').value ='';
-    document.getElementById('n2').value ='';
-    document.getElementById('resultado').innerHTML ='';
+    conteudoDiv.style.display = 'none';
+    conteudoDiv.innerHTML ='';
 }
